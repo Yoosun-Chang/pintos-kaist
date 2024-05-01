9d088c430ee0ff9d85a623ca7b9bf9f2cc47d2d1
Yoosun-Chang <12201321@inha.edu>
2024-05-01 03:01:02 +0000

    Feat: sema_down() 함수 수정(#4)

--- threads/synch.c
+++ threads/synch.c
@@ -66,7 +66,9 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		list_push_back (&sema->waiters, &thread_current ()->elem);
+		/** project1-Synchronizatio */
+		// list_push_back (&sema->waiters, &thread_current ()->elem);
+		list_insert_ordered(&sema->waiters, &thread_current()->elem, cmp_sem_priority, NULL);
 		thread_block ();
 	}
 	sema->value--;
