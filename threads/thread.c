44b1c1f2fb67d29b7f55c8e43c764a16d5686405
Yoosun-Chang <12201321@inha.edu>
2024-05-02 13:37:41 +0000

    Feat: init_thread() 함수 수정(#7)

--- threads/thread.c
+++ threads/thread.c
@@ -422,7 +422,12 @@ init_thread (struct thread *t, const char *name, int priority) {
 	t->status = THREAD_BLOCKED;
 	strlcpy (t->name, name, sizeof t->name);
 	t->tf.rsp = (uint64_t) t + PGSIZE - sizeof (void *);
-	t->priority = priority;
+
+	/** project1-Priority Inversion Problem */
+    t->priority = t->original_priority = priority;
+    list_init(&t->donations);
+    t->wait_lock = NULL;
+
 	t->magic = THREAD_MAGIC;
 }
 
