b314ca13589230c30f52ef9464f2abfc8869dfc2
Yoosun-Chang <12201321@inha.edu>
2024-05-03 02:36:48 +0000

    Feat: thread_exit() 함수 수정(#9)

--- threads/thread.c
+++ threads/thread.c
@@ -311,7 +311,9 @@ thread_exit (void) {
 #ifdef USERPROG
 	process_exit ();
 #endif
-
+	/** project1-Advanced Scheduler */
+	if (thread_mlfqs)
+        list_remove(&thread_current()->all_elem);
 	/* Just set our status to dying and schedule another process.
 	   We will be destroyed during the call to schedule_tail(). */
 	intr_disable ();
