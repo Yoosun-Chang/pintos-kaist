0d03558ec750d61b2beab11b70ee32606fd47644
Yoosun-Chang <12201321@inha.edu>
2024-05-06 01:28:50 +0000

    Feat: System Call 함수 선언(#13)

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -3,4 +3,12 @@
 
 void syscall_init (void);
 
+/** project2-System Call */
+#include <stdbool.h>
+void check_address(void *addr);
+void halt(void);
+void exit(int status);
+bool create(const char *file, unsigned initial_size);
+bool remove(const char *file);
+
 #endif /* userprog/syscall.h */
