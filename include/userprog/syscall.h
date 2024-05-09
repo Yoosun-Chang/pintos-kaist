7b68fdea76c008e37f2ae96dc1f98cb4e7b8abc9
Yoosun-Chang <12201321@inha.edu>
2024-05-09 01:51:31 +0000

    Feat: 구현할 시스템콜 선언(#15)

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -11,6 +11,8 @@ void exit(int status);
 bool create(const char *file, unsigned initial_size);
 bool remove(const char *file);
 
+int open(const char *file);
+int filesize(int fd);
 int read(int fd, void *buffer, unsigned length);
 int write(int fd, const void *buffer, unsigned length);
 void seek(int fd, unsigned position);
