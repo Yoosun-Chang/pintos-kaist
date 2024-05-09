eedf1b30c4a5d1417bfc615f0aa42b7979a4e744
Yoosun-Chang <12201321@inha.edu>
2024-05-09 07:21:52 +0000

    Refactor: 구조 수정

--- userprog/exception.c
+++ userprog/exception.c
@@ -139,6 +139,8 @@ page_fault (struct intr_frame *f) {
 	not_present = (f->error_code & PF_P) == 0;
 	write = (f->error_code & PF_W) != 0;
 	user = (f->error_code & PF_U) != 0;
+
+	/** project2-System Call */
 	exit(-1);
 
 #ifdef VM
