c1bb775e8a01365036c1236db6c13a432a476260
Yoosun-Chang <12201321@inha.edu>
2024-05-10 09:25:53 +0000

    Feat: 공유 자원 검증용 구조체 추가(#19)

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -26,4 +26,11 @@ process_insert_file(int fd, struct file *f);
 #define STDOUT 2
 #define STDERR 3
 
+#define DICTLEN 100
+
+struct dict_elem{
+	uintptr_t key;
+	uintptr_t value;
+};
+
 #endif /* userprog/process.h */
