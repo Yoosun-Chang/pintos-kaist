9dded92fda19ba800110c4c564afbdcb927dcc46
Yoosun-Chang <12201321@inha.edu>
2024-05-24 02:18:03 +0000

    Feat: supplemental_page_table_init() 구현(#21)

--- vm/vm.c
+++ vm/vm.c
@@ -174,6 +174,8 @@ vm_do_claim_page (struct page *page) {
 /* Initialize new supplemental page table */
 void
 supplemental_page_table_init (struct supplemental_page_table *spt UNUSED) {
+	/** Project 3-Memory Management */
+	hash_init(spt, page_hash, page_less, NULL);
 }
 
 /* Copy supplemental page table from src to dst */
