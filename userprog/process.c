74fba0272c78b6d3fe6452c70156714da81345e8
Yoosun-Chang <12201321@inha.edu>
2024-05-24 03:51:28 +0000

    Feat: load_segment() 수정(#23)

--- userprog/process.c
+++ userprog/process.c
@@ -697,16 +697,23 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 		size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
 		size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
-		/* TODO: Set up aux to pass information to the lazy_load_segment. */
-		void *aux = NULL;
+		/** #project3-Anonymous Page */
+		struct vm_load_arg *con = (struct vm_load_arg *)malloc(sizeof(struct vm_load_arg));
+		con->file = file;
+		con->ofs = ofs;
+		con->read_bytes = page_read_bytes;
+
 		if (!vm_alloc_page_with_initializer (VM_ANON, upage,
-					writable, lazy_load_segment, aux))
+					writable, lazy_load_segment, con))
 			return false;
 
 		/* Advance. */
 		read_bytes -= page_read_bytes;
 		zero_bytes -= page_zero_bytes;
 		upage += PGSIZE;
+
+		/** #project3-Anonymous Page */
+		ofs += page_read_bytes;
 	}
 	return true;
 }
