41af6fff300b613e5b377044b14406fd98455b6d
Yoosun-Chang <12201321@inha.edu>
2024-05-24 05:38:33 +0000

    Fix: check_address() 및 page_fault() 수정

--- userprog/exception.c
+++ userprog/exception.c
@@ -154,7 +154,8 @@ page_fault (struct intr_frame *f) {
 
 	/* Count page faults. */
 	page_fault_cnt++;
-
+	exit(-1); /** Project 3-Anonymous Page */
+	
 	/* If the fault is true fault, show info and exit. */
 	printf ("Page fault at %p: %s error %s page in %s context.\n",
 			fault_addr,
--- userprog/syscall.c
+++ userprog/syscall.c
@@ -125,7 +125,7 @@ void check_address(void *addr) {
 struct page *check_address(void *addr) {
     struct thread *curr = thread_current();
 
-    if (is_kernel_vaddr(addr) || addr == NULL)
+    if (is_kernel_vaddr(addr) || addr == NULL || !spt_find_page(&curr->spt, addr))
         exit(-1);
 
     return spt_find_page(&curr->spt, addr);
